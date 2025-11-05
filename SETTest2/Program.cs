814d7c657e185a0f9a11cb8827cf2961a0445997
Madushika-Shiromani <madushikas@embla.asia>
2025-11-05 21:45:21 +0530

    publish 4

--- SETTest2/Program.cs
+++ SETTest2/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SETTest2;
@@ -5,6 +6,18 @@ using SETTest2;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+var baseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+if (baseAddress.AbsoluteUri.Contains("github.io"))
+{
+    // Use hash routing for GitHub Pages
+    builder.Services.AddScoped<NavigationManager>(sp =>
+    {
+        var navigation = sp.GetRequiredService<NavigationManager>();
+        // Handle hash routing
+        return navigation;
+    });
+}
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
